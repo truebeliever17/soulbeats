1b8956262b266565aa477aa40d76203c1c1d5ce3
Madiyar Mukushev <madiyar2000@gmail.com>
2020-12-04 21:33:45 +0600

    Initialize server and add default route

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+require('dotenv').config()
+
+const app = express();
+
+app.use(function (req, res, next) {  
+    res.header("Access-Control-Allow-Origin", "*");  
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");  
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');  
+    next();
+});
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.json({
+        'message': '✌'
+    });
+});
+
+const server = app.listen(process.env.PORT || 5000, function() {
+    const port = server.address().port;
+    console.log("Server is running on port", port);
+});
+
+
