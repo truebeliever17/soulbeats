74298ebf6c340b625f20ac4ad6631a3a86c4ad3b
Madiyar Mukushev <madiyar2000@gmail.com>
2020-12-05 22:45:05 +0600

    Add options for tedious in config

--- backend/connection/db.js
+++ backend/connection/db.js
@@ -1,18 +1,26 @@
-const sql = require('mssql');
+const sql = require("mssql");
 const config = {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     server: process.env.DB_HOST || "localhost",
-    database: process.env.DB_DATABASE
+    database: process.env.DB_DATABASE,
+    options: {
+        encrypt: true,
+        enableArithAbort: true,
+    },
 };
 
 const poolPromise = new sql.ConnectionPool(config)
     .connect()
-    .then(pool => {
+    .then((pool) => {
         console.log("Connected to MSSQL.");
         return pool;
-    }).catch(err => console.log('Database Connection Failed! Bad Config: ', err));
+    })
+    .catch((err) =>
+        console.log("Database Connection Failed! Bad Config: ", err)
+    );
 
 module.exports = {
-    sql, poolPromise
+    sql,
+    poolPromise,
 };
