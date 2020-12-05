1b7350f1617a87d3d015a0b6c1309b47fa0a9095
Madiyar Mukushev <madiyar2000@gmail.com>
2020-12-05 18:49:51 +0600

    Add auth middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,18 @@
+const jwt = require("jsonwebtoken");
+
+module.exports = function (req, res, next) {
+    const token = req.header("token");
+    if (!token) return res.status(401).json({ message: "Auth Error" });
+
+    try {
+        const decoded = jwt.verify(
+            token,
+            process.env.SECRET_KEY || "something"
+        );
+        req.user_id = decoded.user_id;
+        next();
+    } catch (e) {
+        console.error(e);
+        res.status(500).send({ message: "Invalid Token" });
+    }
+};
