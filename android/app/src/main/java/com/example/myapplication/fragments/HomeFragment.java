8b9bacc5fa14d1e97bebb5f9d378956503287447
Madiyar Mukushev <madiyar2000@gmail.com>
2020-12-06 15:01:12 +0600

    Add fetch albums function

--- android/app/src/main/java/com/example/myapplication/fragments/HomeFragment.java
+++ android/app/src/main/java/com/example/myapplication/fragments/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.example.myapplication.fragments;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,10 +11,49 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.R;
+import com.example.myapplication.models.Album;
+import com.example.myapplication.network.NetworkService;
+
+import java.util.List;
+import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class HomeFragment extends Fragment {
+    private boolean isDetached;
+    private static List<Album> albums;
+
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
+        fetchAlbums();
         return view;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        this.isDetached = true;
+    }
+
+    public void fetchAlbums() {
+        NetworkService
+            .getInstance()
+            .getJSONApi()
+            .getAllAlbums()
+            .enqueue(new Callback<List<Album>>() {
+                @Override
+                public void onResponse(Call<List<Album>> call, Response<List<Album>> response) {
+                    if (isDetached) return;
+                    albums = response.body();
+                }
+
+                @Override
+                public void onFailure(Call<List<Album>> call, Throwable t) {
+                    if (isDetached) return;
+                    Log.d("ERROR", Objects.requireNonNull(t.getMessage()));
+                }
+            });
     }
 }
