941a8c29dc653b42947f86c93fa3241e19c2049c
Madiyar Mukushev <madiyar2000@gmail.com>
2020-12-06 17:04:13 +0600

    Add function that configures recyclerview and some minor changes

--- android/app/src/main/java/com/example/myapplication/fragments/HomeFragment.java
+++ android/app/src/main/java/com/example/myapplication/fragments/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -9,10 +10,14 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.myapplication.R;
+import com.example.myapplication.adapters.AlbumsAdapter;
 import com.example.myapplication.models.Album;
 import com.example.myapplication.models.AlbumMainInfo;
+import com.example.myapplication.models.AlbumRequest;
 import com.example.myapplication.network.NetworkService;
 
 import java.util.List;
@@ -25,10 +30,18 @@ import retrofit2.Response;
 public class HomeFragment extends Fragment {
     private boolean isDetached;
     private static List<AlbumMainInfo> albums;
+    private Context context;
+    private RecyclerView albumsRecycler;
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
-        fetchAlbums();
+        context = view.getContext();
+        albumsRecycler = view.findViewById(R.id.albums_rv);
+        if (albums == null) {
+            fetchAlbums();
+        } else {
+            configureAlbumsRecycler();
+        }
         return view;
     }
 
@@ -42,12 +55,13 @@ public class HomeFragment extends Fragment {
         NetworkService
             .getInstance()
             .getJSONApi()
-            .getAlbumsWithMainInfo()
+            .getAlbumsWithMainInfo(new AlbumRequest(true))
             .enqueue(new Callback<List<AlbumMainInfo>>() {
                 @Override
                 public void onResponse(Call<List<AlbumMainInfo>> call, Response<List<AlbumMainInfo>> response) {
                     if (isDetached) return;
                     albums = response.body();
+                    configureAlbumsRecycler();
                 }
 
                 @Override
@@ -56,5 +70,13 @@ public class HomeFragment extends Fragment {
                     Log.d("ERROR", Objects.requireNonNull(t.getMessage()));
                 }
             });
+    }
+
+    private void configureAlbumsRecycler() {
+        if (albums == null || albums.size() == 0) return;
+        AlbumsAdapter albumsAdapter = new AlbumsAdapter(albums);
+        albumsRecycler.setAdapter(albumsAdapter);
+        albumsRecycler.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
+        albumsRecycler.setHasFixedSize(true);
     }
 }
