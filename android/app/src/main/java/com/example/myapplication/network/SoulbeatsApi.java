32241d5c18ca7834bf14f1fbbd0232e579f15129
Madiyar Mukushev <madiyar2000@gmail.com>
2020-12-06 17:05:48 +0600

    Add post method for getting albums with main info

--- android/app/src/main/java/com/example/myapplication/network/SoulbeatsApi.java
+++ android/app/src/main/java/com/example/myapplication/network/SoulbeatsApi.java
@@ -1,13 +1,20 @@
 package com.example.myapplication.network;
 
 import com.example.myapplication.models.Album;
+import com.example.myapplication.models.AlbumMainInfo;
+import com.example.myapplication.models.AlbumRequest;
 
 import java.util.List;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface SoulbeatsApi {
     @GET("/albums")
     Call<List<Album>> getAllAlbums();
+
+    @POST("/albums")
+    Call<List<AlbumMainInfo>> getAlbumsWithMainInfo(@Body AlbumRequest albumRequest);
 }
