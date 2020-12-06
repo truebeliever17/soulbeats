f29579f7629e7b8e136c47752abfb4c494b625fe
Madiyar Mukushev <madiyar2000@gmail.com>
2020-12-06 14:08:44 +0600

    Add singleton network service

---
+++ android/app/src/main/java/com/example/myapplication/network/NetworkService.java
@@ -0,0 +1,28 @@
+package com.example.myapplication.network;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class NetworkService {
+    private static NetworkService instance;
+    private static final String BASE_URL = "http://localhost:5000/";
+    private Retrofit retrofit;
+
+    private NetworkService() {
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public static NetworkService getInstance() {
+        if (instance == null) {
+            instance = new NetworkService();
+        }
+        return instance;
+    }
+
+    public SoulbeatsApi getJSONApi() {
+        return retrofit.create(SoulbeatsApi.class);
+    }
+}
