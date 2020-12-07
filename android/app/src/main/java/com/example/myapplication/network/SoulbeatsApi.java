9d47385b95fe5e0fbe423874a47a5a4a90a305e1
Madiyar Mukushev <madiyar2000@gmail.com>
2020-12-07 08:46:15 +0600

    Add get method to get user by token

--- android/app/src/main/java/com/example/myapplication/network/SoulbeatsApi.java
+++ android/app/src/main/java/com/example/myapplication/network/SoulbeatsApi.java
@@ -3,12 +3,14 @@ package com.example.myapplication.network;
 import com.example.myapplication.models.Album;
 import com.example.myapplication.models.AlbumMainInfo;
 import com.example.myapplication.models.AlbumRequest;
+import com.example.myapplication.models.User;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
 
 public interface SoulbeatsApi {
@@ -17,4 +19,7 @@ public interface SoulbeatsApi {
 
     @POST("/albums")
     Call<List<AlbumMainInfo>> getAlbumsWithMainInfo(@Body AlbumRequest albumRequest);
+
+    @GET("/users/me")
+    Call<User> getUserByToken(@Header("token") String token);
 }
